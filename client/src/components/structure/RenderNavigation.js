fa35a6ec7633595474751daa7bcfcb948126a77b
Irfaan Ahmed <irfaanahmed96@yahoo.com.sg>
2023-10-12 01:13:08 +0800

    Update RenderNavigation.js

--- client/src/components/structure/RenderNavigation.js
+++ client/src/components/structure/RenderNavigation.js
@@ -1,14 +1,7 @@
 import React from "react";
 import { Link, Route, Routes, useLocation } from "react-router-dom";
 import { Menu } from "antd"; // Import Ant Design Menu component
-import {
-  HomeOutlined,
-  LogoutOutlined,
-  LoginOutlined,
-  InfoCircleOutlined,
-  UserOutlined,
-  LockOutlined,
-} from "@ant-design/icons"; // Import Ant Design icons
+import { LogoutOutlined, LoginOutlined } from "@ant-design/icons"; // Import Ant Design icons
 import { AuthData } from "../../auth/AuthWrapper";
 import { nav } from "./navigation";
 import { useNavigate } from "react-router-dom";
