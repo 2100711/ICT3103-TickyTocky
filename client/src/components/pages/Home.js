5e67c2b5116b011c69cce8c68b0ff64b9e31e450
Irfaan Ahmed <irfaanahmed96@yahoo.com.sg>
2023-10-12 03:21:41 +0800

    UI Update
    - Added UI for Home
    - Modified Login CSS

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { Input } from "antd";
+import "../styles/Home.css"; // Corrected the import statement
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="search-bar">
+        <Input placeholder="Search..." className="search-input" allowClear />
+      </div>
+    </div>
+  );
 };
