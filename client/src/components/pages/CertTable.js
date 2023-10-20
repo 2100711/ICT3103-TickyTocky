2028ae9c279df936a415a18888bb61a5e39b1528
ZafrullaKamil <2100764@sit.singaporetech.edu.sg>
2023-10-20 21:58:38 +0800

    temp workaround for viewing pdf

--- client/src/components/pages/CertTable.js
+++ client/src/components/pages/CertTable.js
@@ -3,137 +3,146 @@ import { Table, Button, Pagination, Input } from "antd";
 import { getAllCerts } from "../../api/certs";
 
 export const CertTable = () => {
-    const [certs, setCerts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [pageSize] = useState(2); // Keep the pageSize constant at 2
-    const [searchText, setSearchText] = useState("");
-    const [originalCerts, setOriginalCerts] = useState([]); // Store the original certificates data
+  const [certs, setCerts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize] = useState(2); // Keep the pageSize constant at 2
+  const [searchText, setSearchText] = useState("");
+  const [originalCerts, setOriginalCerts] = useState([]); // Store the original certificates data
 
-    const columns = [{
-            title: "Certificate ID",
-            dataIndex: "cert_id",
-            key: "cert_id",
-            sorter: (a, b) => a.cert_id.localeCompare(b.cert_id),
-            sortDirections: ["ascend", "descend"],
-        },
-        {
-            title: "User Email",
-            dataIndex: "user_email",
-            key: "user_email",
-            sorter: (a, b) => a.user_email.localeCompare(b.user_email),
-            sortDirections: ["ascend", "descend"],
-        },
-        {
-            title: "Actions",
-            dataIndex: "actions",
-            key: "actions",
-            render: (text, cert) => (
-                <div>
-                    <Button type="primary" onClick={() => handleViewPDF(cert)}>
-                        View PDF
-                    </Button>
-                    <Button type="primary" onClick={() => handleDownloadPDF(cert)}>
-                        Download PDF
-                    </Button>
-                </div>
-            ),
-        },
-    ];
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setLoading(true);
-                const response = await getAllCerts();
-                const data = response.certs;
-                console.log("Data: ", data);
-                setCerts(data);
-                setOriginalCerts(data); // Store the original data
-                setLoading(false);
-            } catch (error) {
-                console.error(error);
-                setLoading(false);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    const handleViewPDF = (cert) => {
-        // Access the pdf_content from the cert object
-        console.log("Certificate Data: ", cert);
-        const pdfDataURL = `data:application/pdf;base64,${cert.pdf_content}`;
-        window.open(pdfDataURL, "_blank");
-    };
-
-    const handleDownloadPDF = (cert) => {
-        // Access the pdf_content from the cert object
-        const pdfDataURL = `data:application/pdf;base64,${cert.pdf_content}`;
-        const a = document.createElement("a");
-        a.href = pdfDataURL;
-        a.download = `${cert.cert_id}.pdf`; // You can customize the filename here
-        a.style.display = "none";
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-    };
-
-    const handlePageChange = (page) => {
-        setCurrentPage(page);
-    };
-
-    const handleSearch = (value) => {
-        setSearchText(value);
-        filterCertificates(value);
-    };
-
-    const filterCertificates = (searchText) => {
-        if (searchText) {
-            // Convert the search text to lowercase for a case-insensitive search
-            const lowerCaseSearchText = searchText.toLowerCase();
-
-            // If the search field is not empty, filter the certificates
-            const filteredCerts = originalCerts.filter((cert) => {
-                // Convert certificate data to lowercase for comparison
-                const lowerCaseCertId = cert.cert_id.toLowerCase();
-                const lowerCaseUserEmail = cert.user_email.toLowerCase();
-
-                return (
-                    lowerCaseCertId.includes(lowerCaseSearchText) ||
-                    lowerCaseUserEmail.includes(lowerCaseSearchText)
-                );
-            });
-            setCerts(filteredCerts);
-        } else {
-            // If the search field is empty, reset to the original data
-            setCerts(originalCerts);
-        }
-        setCurrentPage(1); // Reset the page to 1 after a search
-    };
-
-
-    return (
+  const columns = [
+    {
+      title: "Certificate ID",
+      dataIndex: "cert_id",
+      key: "cert_id",
+      sorter: (a, b) => a.cert_id.localeCompare(b.cert_id),
+      sortDirections: ["ascend", "descend"],
+    },
+    {
+      title: "User Email",
+      dataIndex: "user_email",
+      key: "user_email",
+      sorter: (a, b) => a.user_email.localeCompare(b.user_email),
+      sortDirections: ["ascend", "descend"],
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      key: "actions",
+      render: (text, cert) => (
         <div>
-            <Input
-                placeholder="Search by Certificate ID or User Email"
-                value={searchText}
-                onChange={(e) => handleSearch(e.target.value)}
-            />
-            <Table
-                columns={columns}
-                dataSource={certs}
-                loading={loading}
-                rowKey="cert_id"
-                pagination={{
-                    current: currentPage,
-                    pageSize: pageSize,
-                    total: certs.length,
-                    showSizeChanger: true,
-                    showQuickJumper: true,
-                    onChange: handlePageChange,
-                }}
-            />
+          <Button type="primary" onClick={() => handleViewPDF(cert)}>
+            View PDF
+          </Button>
+          <Button type="primary" onClick={() => handleDownloadPDF(cert)}>
+            Download PDF
+          </Button>
         </div>
+      ),
+    },
+  ];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await getAllCerts();
+        const data = response.certs;
+        console.log("Data: ", data);
+        setCerts(data);
+        setOriginalCerts(data); // Store the original data
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // const handleViewPDF = (cert) => {
+  //     // Access the pdf_content from the cert object
+  //     console.log("Certificate Data: ", cert);
+  //     const pdfDataURL = `data:application/pdf;base64,${cert.pdf_content}`;
+  //     window.open(pdfDataURL, "_blank");
+  // };
+
+  const handleViewPDF = (cert) => {
+    // Assuming cert.pdf_content is the actual content of the PDF
+    const pdfDataURL = `data:application/pdf;base64,${cert.pdf_content}`;
+    const newTab = window.open();
+    newTab.document.write(
+      '<iframe width="100%" height="100%" src="' + pdfDataURL + '"></iframe>'
     );
+  };
+
+  const handleDownloadPDF = (cert) => {
+    // Access the pdf_content from the cert object
+    const pdfDataURL = `data:application/pdf;base64,${cert.pdf_content}`;
+    const a = document.createElement("a");
+    a.href = pdfDataURL;
+    a.download = `${cert.cert_id}.pdf`; // You can customize the filename here
+    a.style.display = "none";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const handleSearch = (value) => {
+    setSearchText(value);
+    filterCertificates(value);
+  };
+
+  const filterCertificates = (searchText) => {
+    if (searchText) {
+      // Convert the search text to lowercase for a case-insensitive search
+      const lowerCaseSearchText = searchText.toLowerCase();
+
+      // If the search field is not empty, filter the certificates
+      const filteredCerts = originalCerts.filter((cert) => {
+        // Convert certificate data to lowercase for comparison
+        const lowerCaseCertId = cert.cert_id.toLowerCase();
+        const lowerCaseUserEmail = cert.user_email.toLowerCase();
+
+        return (
+          lowerCaseCertId.includes(lowerCaseSearchText) ||
+          lowerCaseUserEmail.includes(lowerCaseSearchText)
+        );
+      });
+      setCerts(filteredCerts);
+    } else {
+      // If the search field is empty, reset to the original data
+      setCerts(originalCerts);
+    }
+    setCurrentPage(1); // Reset the page to 1 after a search
+  };
+
+  return (
+    <div>
+      <Input
+        placeholder="Search by Certificate ID or User Email"
+        value={searchText}
+        onChange={(e) => handleSearch(e.target.value)}
+      />
+      <Table
+        columns={columns}
+        dataSource={certs}
+        loading={loading}
+        rowKey="cert_id"
+        pagination={{
+          current: currentPage,
+          pageSize: pageSize,
+          total: certs.length,
+          showSizeChanger: true,
+          showQuickJumper: true,
+          onChange: handlePageChange,
+        }}
+      />
+    </div>
+  );
 };
--- server/src/controls/pdf.js
+++ server/src/controls/pdf.js
@@ -3,28 +3,28 @@ import { CertModel } from "../models/Certs.js";
 import fs from "fs";
 
 async function createPdfContent(data) {
-    // Create a new PDF document
-    const pdfDoc = await PDFDocument.create();
+  // Create a new PDF document
+  const pdfDoc = await PDFDocument.create();
 
-    // Add a new page
-    const page = pdfDoc.addPage([600, 400]);
+  // Add a new page
+  const page = pdfDoc.addPage([600, 400]);
 
-    const watermarkImageBytes = await fetchWatermarkImage();
+  const watermarkImageBytes = await fetchWatermarkImage();
 
-    // Add content to the page
-    const { width, height } = page.getSize();
-    const fontSize = 30;
-    const text = "Certificate of Validation";
+  // Add content to the page
+  const { width, height } = page.getSize();
+  const fontSize = 30;
+  const text = "Certificate of Validation";
 
-    page.drawText(text, {
-        x: 50,
-        y: height - 50,
-        size: fontSize,
-        color: rgb(0, 0, 0),
-    });
+  page.drawText(text, {
+    x: 50,
+    y: height - 50,
+    size: fontSize,
+    color: rgb(0, 0, 0),
+  });
 
-    // Add certificate data
-    const certData = `
+  // Add certificate data
+  const certData = `
         Certificate ID: ${data.cert_id}
         User Email: ${data.user_email}
         Validated By: ${data.validated_by}
@@ -35,36 +35,36 @@ async function createPdfContent(data) {
         Remarks: ${data.remarks}
     `;
 
-    page.drawText(certData, {
-        x: 50,
-        y: height - 150,
-        size: 14,
-        color: rgb(0, 0, 0),
-    });
+  page.drawText(certData, {
+    x: 50,
+    y: height - 150,
+    size: 14,
+    color: rgb(0, 0, 0),
+  });
 
-    // Add watermark text
-    const watermarkImage = await pdfDoc.embedPng(watermarkImageBytes);
-    const imageWidth = 200; // Adjust the width of the image as needed
-    const imageHeight = 100; // Adjust the height of the image as needed
-    const imageX = width / 2 - imageWidth / 2;
-    const imageY = height / 2 - imageHeight / 2;
+  // Add watermark text
+  const watermarkImage = await pdfDoc.embedPng(watermarkImageBytes);
+  const imageWidth = 200; // Adjust the width of the image as needed
+  const imageHeight = 100; // Adjust the height of the image as needed
+  const imageX = width / 2 - imageWidth / 2;
+  const imageY = height / 2 - imageHeight / 2;
 
-    page.drawImage(watermarkImage, {
-        x: imageX,
-        y: imageY,
-        width: imageWidth,
-        height: imageHeight,
-        opacity: 0.5, // Adjust opacity as needed
-        rotate: radians(45), // Rotate by 45 degrees
-    });
+  page.drawImage(watermarkImage, {
+    x: imageX,
+    y: imageY,
+    width: imageWidth,
+    height: imageHeight,
+    opacity: 0.5, // Adjust opacity as needed
+    rotate: radians(45), // Rotate by 45 degrees
+  });
 
-    // Serialize the PDF to a byte array
-    const pdfBytes = await pdfDoc.save();
+  // Serialize the PDF to a byte array
+  const pdfBytes = await pdfDoc.save();
 
-    // Convert the byte array to a base64 string
-    const pdfContent = Buffer.from(pdfBytes).toString("base64");
+  // Convert the byte array to a base64 string
+  const pdfContent = Buffer.from(pdfBytes).toString("base64");
 
-    return pdfContent;
+  return pdfContent;
 }
 
 // async function createPdfContents(dataArray) {
@@ -134,10 +134,9 @@ async function createPdfContent(data) {
 // }
 
 async function fetchWatermarkImage() {
-    // Read the image file from a local path
-    const imageBytes = fs.readFileSync('../server/src/img/logo.png');
-    return new Uint8Array(imageBytes);
+  // Read the image file from a local path
+  const imageBytes = fs.readFileSync("../server/src/img/logo.png");
+  return new Uint8Array(imageBytes);
 }
 
-
-export { createPdfContent, createPdfContents };
+export { createPdfContent };
