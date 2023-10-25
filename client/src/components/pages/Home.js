81e2813eaa88166b6dc6fcdfe1147181b96f1729
Irfaan Ahmed <irfaanahmed96@yahoo.com.sg>
2023-10-25 19:59:46 +0800

    Updated Search and Nginx Config

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,12 +1,51 @@
-import React from "react";
-import { Input } from "antd";
+import React, { useState } from "react";
+import { Input, notification } from "antd";
 import "../styles/Home.css";
 
 export const Home = () => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    await fetch("http://localhost:3001/certs/" + searchInput, {
+      method: "GET",
+      credentials: "include",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).then((response) => {
+      console.log(response);
+      response.json().then(() => {
+        if (response.status === 200) {
+          notification.success({
+            message: "Certification Found",
+            description: "Certificate is valid",
+            duration: 5,
+          });
+        } else {
+          notification.error({
+            message: "Certification Not Found",
+            description: "Certificate is invalid",
+            duration: 5,
+          });
+        }
+      });
+    });
+  };
+
   return (
     <div className="home">
       <div className="search-bar">
-        <Input placeholder="Search..." className="search-input" allowClear />
+        <Input
+          placeholder="Search..."
+          className="search-input"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          onPressEnter={handleSearch}
+          allowClear
+        />
       </div>
     </div>
   );
--- server/src/index.js
+++ server/src/index.js
@@ -17,10 +17,10 @@ import { PORT, MONGODB_CONNECTION } from "./constants.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" })); // Comment this out if you are using nginx
 mongoose.connect(MONGODB_CONNECTION, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 // Get the default connection
@@ -28,24 +28,24 @@ const db = mongoose.connection;
 
 // Sessions
 app.use(
-    session({
-        secret: "secret-key-from-env",
-        cookie: {
-            httpOnly: true,
-            signed: true,
-        },
-        resave: false,
-        saveUninitialized: false,
-        store: new MongoStore({
-            client: db.getClient(),
-            crypto: {
-                secret: "squirrel",
-            },
-            autoRemove: "interval",
-            autoRemoveInterval: 1, // checks every 1 minute to delete sessions that have expired
-            ttl: 5 * 60, // sessions last for 5 minutes and session length will be added by 5 from current time if user interacts with backend server
-        }),
-    })
+  session({
+    secret: "secret-key-from-env",
+    cookie: {
+      httpOnly: true,
+      signed: true,
+    },
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+      client: db.getClient(),
+      crypto: {
+        secret: "squirrel",
+      },
+      autoRemove: "interval",
+      autoRemoveInterval: 1, // checks every 1 minute to delete sessions that have expired
+      ttl: 5 * 60, // sessions last for 5 minutes and session length will be added by 5 from current time if user interacts with backend server
+    }),
+  })
 );
 
 // Routes
@@ -59,27 +59,27 @@ app.use("/certs", certRouter);
 
 // Listen for the "connected" event
 db.on("connected", () => {
-    console.log("Mongoose connection is successful!");
+  console.log("Mongoose connection is successful!");
 });
 
 // Listen for the "error" event
 db.on("error", (err) => {
-    console.error("Mongoose connection error:", err);
+  console.error("Mongoose connection error:", err);
 });
 
 // Listen for the "disconnected" event
 db.on("disconnected", () => {
-    console.log("Mongoose connection disconnected");
+  console.log("Mongoose connection disconnected");
 });
 
 app.get("/", (req, res) => res.send("Dockerizing Node Application"));
 
 app.get("/test", async (req, res) => {
-    if (!req.session.user) {
-        return res.status(401).json({ message: "no session" });
-    }
+  if (!req.session.user) {
+    return res.status(401).json({ message: "no session" });
+  }
 
-    return res.status(200).json({ message: req.session });
+  return res.status(200).json({ message: req.session });
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
