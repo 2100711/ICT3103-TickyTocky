b7e48c89f73b4359bd6e736166af00acdec1fbac
joash2808 <joashlwj@gmail.com>
2023-11-01 00:19:06 +0800

    sanitization added

--- server/src/index.js
+++ server/src/index.js
@@ -3,8 +3,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 import session from "express-session";
 import MongoStore from "connect-mongo";
-import helmet from "helmet";
-import crypto from "crypto";
+import helmet from 'helmet'
+import crypto from 'crypto'
 import rateLimit from "express-rate-limit"; //added
 
 import { authRouter } from "./routes/auth.js";
@@ -15,46 +15,46 @@ import { PORT, MONGODB_CONNECTION } from "./constants.js";
 
 const app = express();
 
-// // Helmet middleware for securing HTTP headers
-// app.use(
-//   // sets X-Content-type-options: nosniff (by default)
-//   // sets X-DNS-prefetch-control: off (by default)
-//   // sets X-Permitted-Cross-Domain-Policies: none (by default)
-//   helmet({
-//     contentSecurityPolicy:{
-//       directives: {
-//         scriptSrc: ["'self'", (req, res) => `'nonce-${res.locals.cspNonce}'`],
-//       }
-//     },
-//     // Deny X-Frame-Options
-//     xFrameOptions: {action: "deny"},
+// Helmet middleware for securing HTTP headers
+app.use(
+  // sets X-Content-type-options: nosniff (by default)
+  // sets X-DNS-prefetch-control: off (by default)
+  // sets X-Permitted-Cross-Domain-Policies: none (by default)
+  helmet({
+    contentSecurityPolicy:{
+      directives: {
+        scriptSrc: ["'self'", (req, res) => `'nonce-${res.locals.cspNonce}'`],
+      }
+    },
+    // Deny X-Frame-Options
+    xFrameOptions: {action: "deny"},
 
-//     // HSTS max age; included subdomains
-//     strictTransportSecurity:{
-//       maxAge: 31536000 //set to one year
-//     }
+    // HSTS max age; included subdomains
+    strictTransportSecurity:{
+      maxAge: 31536000 //set to one year
+    }
 
-//   }
-// ));
+  }
+));
 
-// // For Content Security Policy
-// app.use((req, res, next) => {
-//   res.locals.cspNonce = crypto.randomBytes(16).toString('hex');
-//   next();
-// });
+// For Content Security Policy
+app.use((req, res, next) => {
+  res.locals.cspNonce = crypto.randomBytes(16).toString('hex');
+  next();
+});
 
-// // Cache-Control middleware
-// app.use((req, res, next) => {
-//   // Set Cache-Control directives in the HTTP response
-//   res.setHeader('Cache-Control', 'max-age=3600, public'); // Example: Cache response for 1 hour (3600 seconds), allow public caching
-//   next();
-// });
+// Cache-Control middleware
+app.use((req, res, next) => {
+  // Set Cache-Control directives in the HTTP response
+  res.setHeader('Cache-Control', 'max-age=3600, public'); // Example: Cache response for 1 hour (3600 seconds), allow public caching
+  next();
+});
 
 app.use(express.json());
 app.use(cors({ credentials: true, origin: "http://localhost:3000" })); // Comment this out if you are using nginx
 mongoose.connect(MONGODB_CONNECTION, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 // Get the default connection
@@ -62,30 +62,30 @@ const db = mongoose.connection;
 
 // Sessions
 app.use(
-  session({
-    secret: "secret-key-from-env",
-    cookie: {
-      httpOnly: true,
-      signed: true,
-    },
-    resave: false,
-    saveUninitialized: false,
-    store: new MongoStore({
-      client: db.getClient(),
-      crypto: {
-        secret: "squirrel",
-      },
-      autoRemove: "interval",
-      autoRemoveInterval: 1, // checks every 1 minute to delete sessions that have expired
-      ttl: 5 * 60, // sessions last for 5 minutes and session length will be added by 5 from current time if user interacts with backend server
-    }),
-  })
+    session({
+        secret: "secret-key-from-env",
+        cookie: {
+            httpOnly: true,
+            signed: true,
+        },
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({
+            client: db.getClient(),
+            crypto: {
+                secret: "squirrel",
+            },
+            autoRemove: "interval",
+            autoRemoveInterval: 1, // checks every 1 minute to delete sessions that have expired
+            ttl: 5 * 60, // sessions last for 5 minutes and session length will be added by 5 from current time if user interacts with backend server
+        }),
+    })
 );
 
 const apiLimiter = rateLimit({
-  windowMs: 1 * 1000, // 1 second
-  max: 5, // limit each IP to 5 requests per windowMs
-  message: "Too many requests, please try again later.",
+    windowMs: 1 * 1000, // 1 second
+    max: 5, // limit each IP to 5 requests per windowMs
+    message: "Too many requests, please try again later."
 });
 
 app.use(apiLimiter); // Apply rate limiter middleware to all routes
@@ -97,27 +97,27 @@ app.use("/certs", certRouter);
 
 // Listen for the "connected" event
 db.on("connected", () => {
-  console.log("Mongoose connection is successful!");
+    console.log("Mongoose connection is successful!");
 });
 
 // Listen for the "error" event
 db.on("error", (err) => {
-  console.error("Mongoose connection error:", err);
+    console.error("Mongoose connection error:", err);
 });
 
 // Listen for the "disconnected" event
 db.on("disconnected", () => {
-  console.log("Mongoose connection disconnected");
+    console.log("Mongoose connection disconnected");
 });
 
 app.get("/", (req, res) => res.send("Dockerizing Node Application"));
 
 app.get("/test", async (req, res) => {
-  if (!req.session.user) {
-    return res.status(401).json({ message: "no session" });
-  }
+    if (!req.session.user) {
+        return res.status(401).json({ message: "no session" });
+    }
 
-  return res.status(200).json({ message: req.session });
+    return res.status(200).json({ message: req.session });
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
