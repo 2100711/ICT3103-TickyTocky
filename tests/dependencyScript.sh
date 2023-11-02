377151df85c8b7028eaaa11460032a9fece26aaa
Terence2389 <94297073+Terence2389@users.noreply.github.com>
2023-11-03 03:23:37 +0800

    Update Script

--- tests/dependencyScript.sh
+++ tests/dependencyScript.sh
@@ -13,25 +13,25 @@ apt-get install -y python3-pip
 #pip3 install selenium
 
 # Install Google Chrome (replace with your preferred browser)
-# Adding trusting keys to apt for repositories
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+# # Adding trusting keys to apt for repositories
+# wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 
-# Adding Google Chrome to the repositories
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+# # Adding Google Chrome to the repositories
+# sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
 apt-get update -y
-apt-get install chromium-chromedriver
-sh chromedriver -v
+# apt-get install chromium-chromedriver
 
 # # Install ChromeDriver
 # # Download the appropriate version of ChromeDriver and install it
-# CHROME_DRIVER_VERSION="your_chromedriver_version"
-# wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
-# unzip ~/chromedriver_linux64.zip -d ~/
-# mv -f ~/chromedriver /usr/local/bin/chromedriver
-# chmod +x /usr/local/bin/chromedriver
-# rm ~/chromedriver_linux64.zip
+CHROME_DRIVER_VERSION="100.0.4896.20"
+wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
+unzip ~/chromedriver_linux64.zip -d ~/
+mv -f ~/chromedriver /usr/local/bin/chromedriver
+chmod +x /usr/local/bin/chromedriver
+rm ~/chromedriver_linux64.zip
 
+sh chromedriver -v
 # Add ChromeDriver to system PATH (optional)
 # echo "export PATH=\$PATH:/usr/local/bin/chromedriver" >> ~/.bashrc
 
