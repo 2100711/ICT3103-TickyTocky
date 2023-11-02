57e68857bd85cf2796e6e21be0ae3ec74425256e
Terence2389 <94297073+Terence2389@users.noreply.github.com>
2023-11-03 03:05:46 +0800

    Update

--- tests/dependencyScript.sh
+++ tests/dependencyScript.sh
@@ -19,6 +19,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 # Adding Google Chrome to the repositories
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
+apt-get update -y
 apt-get install chromium-chromedriver
 sh chromedriver -v
 
