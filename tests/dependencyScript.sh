fa6bf6a86fcfc1714b4a545e2b7eaed94f422ab1
Terence2389 <94297073+Terence2389@users.noreply.github.com>
2023-11-03 03:02:08 +0800

    Update

--- tests/dependencyScript.sh
+++ tests/dependencyScript.sh
@@ -13,6 +13,12 @@ apt-get install -y python3-pip
 #pip3 install selenium
 
 # Install Google Chrome (replace with your preferred browser)
+# Adding trusting keys to apt for repositories
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+
+# Adding Google Chrome to the repositories
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
 apt-get install chromium-chromedriver
 sh chromedriver -v
 
